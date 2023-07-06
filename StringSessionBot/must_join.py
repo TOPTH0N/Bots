f13535e3100a133799c826c5187b24220e64e4bb
xXStrem <119265616+xXStrem@users.noreply.github.com>
2023-07-06 13:32:23 +0300

    must_join.py

--- StringSessionBot/must_join.py
+++ StringSessionBot/must_join.py
@@ -19,7 +19,7 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"- يـجـب عليـك اެݪاشـتـࢪاك [فـي هـذه الـقناة]({https://t.me/xXStrem}) وبعد ذلـك أعـد المـحاولـة مـࢪة أخـࢪى!",
+                    f"- يـجـب عليـك اެݪاشـتـࢪاك [فـي هـذه الـقناة]({link}) وبعد ذلـك أعـد المـحاولـة مـࢪة أخـࢪى!",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup(
                         [[InlineKeyboardButton("سورس ستريم- xXStrem", url=link)]]
